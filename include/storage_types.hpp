87d679f356c0a1fa51104a3bd310b441381dac14
TopRamen1 <nicolas.duc@op.pl>
2019-12-16 17:52:29 +0100

    Stockplile Queue queue type

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,9 +1,34 @@
 #ifndef NETSIM_STORAGE_TYPES_HPP
 #define NETSIM_STORAGE_TYPES_HPP
 
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+class Package {};
+
+enum PackageQueueType {
+    FIFO,
+    LIFO
+};
+
 class IPackageStockpile {
 public:
+    virtual void push(Package&& package) {};
 
+    virtual bool empty() {};
+
+    virtual std::size_t size() {};
+};
+
+class IPackageQueue : IPackageStockpile {
+    virtual Package pop() {};
+
+    virtual PackageQueueType get_queuetype() {};
 };
 
 #endif //NETSIM_STORAGE_TYPES_HPP
