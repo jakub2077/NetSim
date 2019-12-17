e865a2e50b1bcb91c2c615f3a10124de502a6982
TopRamen1 <nicolas.duc@op.pl>
2019-12-17 19:18:36 +0100

    Tests config and package files

---
+++ include/package.hpp
@@ -0,0 +1,8 @@
+//
+// Created by PC on 17.12.2019.
+//
+
+#ifndef NETSIM_PACKAGE_HPP
+#define NETSIM_PACKAGE_HPP
+
+#endif //NETSIM_PACKAGE_HPP
---
+++ tests/package_tests.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+#include ".hpp"
+
+TEST(MatlabVectorTest, add) {
+
+}
