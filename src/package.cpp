085b1167442b93ee74f78a2e3b9629846d997d02
KBakalarz <58993050+KBakalarz@users.noreply.github.com>
2019-12-18 17:46:56 +0100

    Update package.cpp

--- src/package.cpp
+++ src/package.cpp
@@ -7,21 +7,24 @@ std::set<ElementID> Package::freed_IDs = {};
 Package::Package() {
     if (not freed_IDs.empty()){
         ElementID first_free_ID = *freed_IDs.begin();
-        ID_ = first_free_ID;
+        id_ = first_free_ID;
         freed_IDs.erase(freed_IDs.begin());
         assigned_IDs.insert(first_free_ID);
     }
     else if (not assigned_IDs.empty()){
         ElementID new_ID= *freed_IDs.end() + 1;
-        ID_ = new_ID;
+        id_ = new_ID;
     }
     else{
         ElementID new_ID= 0;
-        ID_ = new_ID;
+        id_ = new_ID;
     }
 }
 
 
-Package::Package(Package&& other){
-    this->ID_ = other.ID_;
+Package & Package::operator= (const Package&&) noexcept{
+    ElementID tmp = this->id_;
+    delete this;
+    this->id_ = tmp;
+    return *this;
 }
