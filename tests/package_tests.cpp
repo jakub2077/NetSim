3ac1c7a0673c6ab203088706e67e57b5e9c99d3e
TopRamen1 <nicolas.duc@op.pl>
2019-12-18 22:32:35 +0100

    package tests

--- tests/package_tests.cpp
+++ tests/package_tests.cpp
@@ -1,3 +1,21 @@
 #include "gtest/gtest.h"
 
 #include "package.hpp"
+
+TEST(PackageTest, IDassignmentManual){
+
+    Package* p1 = new Package();
+    Package* p2 = new Package();
+
+    ASSERT_EQ(p1->get_id(),1);
+    ASSERT_EQ(p2->get_id(),2);
+
+    delete p1;
+
+    Package* p3 = new Package();
+
+    ASSERT_EQ(p3->get_id(),1);
+
+    delete p2;
+    delete p3;
+}
