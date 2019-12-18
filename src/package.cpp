2e730bed46a9a843b6d6806b923d5b4ad9faae4b
KBakalarz <58993050+KBakalarz@users.noreply.github.com>
2019-12-18 16:52:50 +0100

    Update package.cpp
    Do zrobienia jeszcze przeciążanie operatora '='

--- src/package.cpp
+++ src/package.cpp
@@ -1,2 +1,27 @@
 #include "package.hpp"
 
+std::set<ElementID> Package::assigned_IDs = {};
+std::set<ElementID> Package::freed_IDs = {};
+
+
+Package::Package() {
+    if (not freed_IDs.empty()){
+        ElementID first_free_ID = *freed_IDs.begin();
+        ID_ = first_free_ID;
+        freed_IDs.erase(freed_IDs.begin());
+        assigned_IDs.insert(first_free_ID);
+    }
+    else if (not assigned_IDs.empty()){
+        ElementID new_ID= *freed_IDs.end() + 1;
+        ID_ = new_ID;
+    }
+    else{
+        ElementID new_ID= 0;
+        ID_ = new_ID;
+    }
+}
+
+
+Package::Package(Package&& other){
+    this->ID_ = other.ID_;
+}
