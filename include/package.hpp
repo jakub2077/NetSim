c572342c0547f2ddf9a049f843787977a45c7f4a
KBakalarz <58993050+KBakalarz@users.noreply.github.com>
2019-12-18 19:46:24 +0100

    Update package.hpp

--- include/package.hpp
+++ include/package.hpp
@@ -10,9 +10,11 @@ class Package {
 public:
     explicit Package();
 
-    Package(Package&& package) noexcept : id_(package.id_) {};
-
-    bool operator==(const Package &rhs) const;
+    ~Package();
+    
+    Package(Package&& package) noexcept : id_(package.id_){};
+    
+    Package & operator =(Package&&) noexcept;
 
     ElementID get_id() const { return id_;};
 private:
