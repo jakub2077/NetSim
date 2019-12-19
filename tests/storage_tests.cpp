c3a3159286ac41ac46d11a1594f764f6abd32a8d
TopRamen1 <49471089+TopRamen1@users.noreply.github.com>
2019-12-19 11:36:49 +0100

    Update storage_tests.cpp

--- tests/storage_tests.cpp
+++ tests/storage_tests.cpp
@@ -2,53 +2,17 @@
 
 #include "storage_types.hpp"
 
-TEST(PackageQueueTest, push_and_pop_FIFO) {
-
-    Package* p1 = new Package();
-    Package* p2 = new Package();
-    Package* p3 = new Package();
-
-    PackageQueue q1(PackageQueueType::FIFO);
-
-    q1.push(std::move(*p1));
-    q1.push(std::move(*p2));
-    q1.push(std::move(*p3));
-
-    EXPECT_EQ(q1.pop().get_id(),1);
-    EXPECT_EQ(q1.pop().get_id(),2);
-    EXPECT_EQ(q1.pop().get_id(),3);
-
-    delete p1;
-    delete p2;
-    delete p3;
-}
-
-TEST(PackageQueueTest, push_and_pop_LIFO) {
-
-    Package* p1 = new Package();
-    Package* p2 = new Package();
-    Package* p3 = new Package();
-
-    PackageQueue q1(PackageQueueType::LIFO);
-
-    q1.push(std::move(*p1));
-    q1.push(std::move(*p2));
-    q1.push(std::move(*p3));
-
-    EXPECT_EQ(q1.pop().get_id(),3);
-    EXPECT_EQ(q1.pop().get_id(),2);
-    EXPECT_EQ(q1.pop().get_id(),1);
-
-    delete p1;
-    delete p2;
-    delete p3;
-}
-
 TEST(PackageQueueTest, PopQueueFIFO){
     PackageQueue PQ_FIFO(PackageQueueType::FIFO);
-    PQ_FIFO.push(Package());
-    PQ_FIFO.push(Package());
-    PQ_FIFO.push(Package());
+
+    Package p1;
+    Package p2;
+    Package p3;
+
+    PQ_FIFO.push(std::move(p1));
+    PQ_FIFO.push(std::move(p2));
+
+    PQ_FIFO.push(std::move(p3));
 
     Package temp = PQ_FIFO.pop();
     Package temp2 = PQ_FIFO.pop();
@@ -59,13 +23,20 @@ TEST(PackageQueueTest, PopQueueFIFO){
 
 TEST(PackageQueueTest, PopQueueLIFO){
     PackageQueue PQ_LIFO(PackageQueueType::LIFO);
-    PQ_LIFO.push(Package());
-    PQ_LIFO.push(Package());
-    PQ_LIFO.push(Package());
+
+    Package p1;
+    Package p2;
+    Package p3;
+
+    PQ_LIFO.push(std::move(p1));
+    PQ_LIFO.push(std::move(p2));
+    PQ_LIFO.push(std::move(p3));
 
     Package temp = PQ_LIFO.pop();
     Package temp2 = PQ_LIFO.pop();
+    Package temp3 = PQ_LIFO.pop();
 
     ASSERT_EQ(temp.get_id(),3);
     ASSERT_EQ(temp2.get_id(),2);
+    ASSERT_EQ(temp3.get_id(),1);
 }
