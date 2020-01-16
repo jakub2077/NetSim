49f13d6e8a7388230c9ff7c6874faa3fd55ee16f
TopRamen1 <49471089+TopRamen1@users.noreply.github.com>
2020-01-16 12:27:22 +0100

    Add files via upload

---
+++ src/factory.cpp
@@ -0,0 +1,35 @@
+#include "factory.hpp"
+
+bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors){
+    if (node_colors[sender] == NodeColor::VERIFIED){
+        return true;
+    }
+    node_colors[sender] = NodeColor::VISITED;
+
+    if (sender->receiver_preferences_.get_preferences().empty()){
+        throw "";
+    }
+
+    bool has_receivers = false;
+    for (auto r: sender->receiver_preferences_.get_preferences()) {
+        if (r.first->get_receiver_type() == ReceiverType::STOREHOUSE) {
+            has_receivers = true;
+        } else if (r.first->get_receiver_type() == ReceiverType::WORKER) {
+            IPackageReceiver* receiver_ptr = r.first;
+            auto worker_ptr = dynamic_cast<Worker*>(receiver_ptr);
+            auto sendrecv_ptr = dynamic_cast<PackageSender*>(worker_ptr);
+
+            if (sendrecv_ptr == sender){
+
+            }
+            has_receivers = false;
+
+            if (node_colors[sendrecv_ptr] != NodeColor::VISITED) {
+                has_receivers = has_reachable_storehouse(sendrecv_ptr, node_colors);
+            }
+
+        }
+
+    }
+
+}
